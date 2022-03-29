a13cc83a38e6c275ad947200813d965f46311d45
Kevin Egstorf <Kevin.Egstorf@gmail.com>
2022-03-29 20:31:10 +0200

    added api call

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,21 @@
+import { useLoaderData } from "@remix-run/react";
+import { useEffect } from "react";
+
+export async function getPokemon(){
+    let res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
+   return res;
+}
+
+export let loader = async () => {
+  return getPokemon();
+}
+
 export default function Index() {
+
+  let pokemon = useLoaderData();
+
+  console.log(pokemon)
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
