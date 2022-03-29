8e21b63be5f33ba7696ecd28ee8a68bc57bc43e4
Fransjo Leihitu <fransyozef@Fransjos-MacBook-Pro.local>
2022-03-29 20:37:56 +0200

    added historyTable component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import { useEffect } from "react";
+import HistoryTable from "~/components/historyTable";
 
 export async function getPokemon(){
     let res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
@@ -18,32 +19,7 @@ export default function Index() {
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <HistoryTable></HistoryTable>
     </div>
   );
 }
