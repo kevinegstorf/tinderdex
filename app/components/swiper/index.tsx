2c16338c007e60af14e204182fe34efdb84f210a
Laura Bogers <lbogers@pancompany.com>
2022-03-29 21:23:29 +0200

    feat(swiper): add swipes

--- app/components/swiper/index.tsx
+++ app/components/swiper/index.tsx
@@ -1,14 +1,27 @@
 import pikachu from './assets/pikachu.jpg';
+import happy from './assets/happy.png';
+import sad from './assets/sad.png';
+
+function swipeLeft() {
+    console.log("You swiped Left !");
+}
+
+function swipeRight() {
+    console.log("You swiped Right !");
+}
 
 export default function Swiper(pokemon: any) {
+    if (!pokemon) return (<div></div>);
+    console.log(pokemon);
     return (
         <main>
+            <div className="flex flex-col">
             <img style={{ width: '300px'}} src={pikachu} alt="Profile Picture">
             </img>
-            <h1>Pikachu</h1>
-            <p>Level 9999</p>
-            <p>Type: Electric</p>
-            <p>Area: Forest</p>
+            <div className="flex flex-row"><h1>Pikachu</h1><h1>Level: 10</h1></div>
+            <div className="flex flex-row"><p>Type: Electric</p></div>
+            </div>
+            <div className="flex flex-row"><img onClick={swipeLeft} style={{width: '80px'}} src={happy}></img><img onClick={swipeRight} style={{width: '80px'}} src={sad}></img></div>
         </main>
     );
 }
